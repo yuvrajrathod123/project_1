18a28898aebe2bbbf59259b51617151293e6af56
Yuvraj Rathod <110601973+yuvrajrathod123@users.noreply.github.com>
2026-07-29 18:35:59 +0000

    Added the logger function

--- backend/src/controller/todoController.js
+++ backend/src/controller/todoController.js
@@ -1,22 +1,28 @@
-const todoModel = require('../model/todoModel')
+const todoModel = require('../model/todoModel');
+const logger = require('../utils/logger')
 
 const createTodo = async (req, res) =>{
     try{
         const {title, description} = req.body;
         if(!title || title.trim()===''){
+
+            logger.warn('Todo creation failed - Title missing', {ip:req.ip, requestbody:req.body});
             return res.status(400).json({
                 error:"Title is required"
             });
         }
 
+        logger.info('Creating new todo', {title:title, description:description});
         const newTodo = await todoModel.createTodo(title, description);
+        logger.info('Todo added succesfully',{todoId:newTodo.id, title:newTodo.id, completionTime: new Date().toISOString()})
 
         return res.status(201).json({
-            message:"todo added succesfully",
+            message:"Todo added succesfully",
             todo: newTodo
         });
 
     }catch(error){
+        logger.error('Error creating todo', {error:error.message, stack:error.stack, requestbody:req.body})
         console.error('Error creating todo', error);
         res.status(500).json({
             error:error.message
